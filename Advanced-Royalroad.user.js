201295ed2fc50b636a3a192547a9aaf3be4cd5f9
TheRedKeks <40245965+RedCommander735@users.noreply.github.com>
2024-04-15 00:04:05 +0200

    fix: version number

--- Advanced-Royalroad.user.js
+++ Advanced-Royalroad.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Advanced Royalroad
 // @namespace    https://github.com/RedCommander735
-// @version      0.1.0
+// @version      0.1.2
 // @description  Makes Royalroad search infinitely scrollable and adds a minimum number of chapters filter
 // @author       RedCommander735
 // @match        https://www.royalroad.com/*
